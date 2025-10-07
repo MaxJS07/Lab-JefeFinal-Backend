c556c360e9dc371af40d8b17024948e309eb95f3
MaxJS07 <maxjimenezsep@gmail.com>
2025-10-07 08:14:36 -0600

    INICIALIZACION DE LA API

---
+++ MaxGabriela2B/src/main/java/LabFinal/MaxGabriela2B/MaxGabriela2BApplication.java
@@ -0,0 +1,13 @@
+package LabFinal.MaxGabriela2B;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaxGabriela2BApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaxGabriela2BApplication.class, args);
+	}
+
+}
---
+++ MaxGabriela2B/src/main/java/LabFinal/MaxGabriela2B/ServletInitializer.java
@@ -0,0 +1,13 @@
+package LabFinal.MaxGabriela2B;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MaxGabriela2BApplication.class);
+	}
+
+}
